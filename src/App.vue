628f7289b5b7bfb60b1bb6943b3e14ba00d6c79e
Thiago Lopes <tiagojosecnn@outlook.com>
2023-09-16 16:42:08 -0300

    Cria componente barra de navegação.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <!-- <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <header>
+    <BarraNavegacao/>
+  </header>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import BarraNavegacao from './components/BarraNavegacao.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BarraNavegacao
   }
 }
 </script>
@@ -21,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/BarraNavegacao.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-warning">
+    <div class="container">
+      <a href="#">
+        <img src="../assets/logoFinans.png" alt="logoNavBar" width="142" />
+      </a>
+
+      <!--Menu hambuger-->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#menu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-between" id="menu">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a href="" class="nav-link">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">Recursos</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">Benefícios</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">Preços</a>
+          </li>
+
+          <li class="nav-item">
+            <a href="" class="nav-link">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "BarraNavegacao",
+};
+</script>
