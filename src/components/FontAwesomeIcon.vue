f4b88800a8c9515999a7b746b657e8b46b5489ae
Thiago Lopes <tiagojosecnn@outlook.com>
2023-09-16 18:19:13 -0300

    Cria componente para icones do FontAwesome.

---
+++ src/components/FontAwesomeIcon.vue
@@ -0,0 +1,17 @@
+<!-- FontAwesomeIcon.vue -->
+<template>
+    <i :class="['fa', iconClass]"></i>
+  </template>
+  
+  <script>
+  export default {
+    name: ' FontAwesomeIcon',
+    props: {
+      iconClass: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
