3c19526ef3b31da72c689781670051cb17b19051
Thiago Lopes <tiagojosecnn@outlook.com>
2023-09-16 17:32:51 -0300

    Corrige rota.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
  <!-- <img alt="Vue logo" src="./assets/logo.png">-->
   <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <header>
-    <BarraNavegacao/>
-  </header>
- 
+  <Home/>
 </template>
 
 <script>
 //import HelloWorld from './components/HelloWorld.vue'
-import BarraNavegacao from './components/BarraNavegacao.vue';
+import Home from './components/pages/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    BarraNavegacao
-  }
+    Home
+}
 }
 </script>
 
