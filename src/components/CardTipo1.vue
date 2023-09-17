8714ed3772d66109088768c015e4fe07fde85f3e
Thiago Lopes <tiagojosecnn@outlook.com>
2023-09-16 23:13:50 -0300

    Cria componente card.

---
+++ src/components/CardTipo1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 d-flex">
+        <div class="align-self-center">
+          <h1 v-if="title">{{ title }}</h1>
+          <p v-if="texto">{{ texto }}</p>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <img :src="path" :alt="title || 'Image Description'" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "CardTipo1",
+        props: {
+            title: String,
+            texto: String,
+            path: {
+                type: [String, Object],
+                default: "",
+            },
+    },
+};
+</script>
