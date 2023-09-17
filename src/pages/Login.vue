600b2d7d990df93655d3c33c47280d2ae3c020b4
Thiago Lopes <tiagojosecnn@outlook.com>
2023-09-17 17:38:33 -0300

    Conclui paginas.

---
+++ src/pages/Login.vue
@@ -0,0 +1,12 @@
+<template>
+   <CardForm heading="Login" />
+</template>
+
+<script>
+    import CardForm from '@/components/CardForm.vue';
+
+    export default {
+    name: 'LoginView',
+    components: { CardForm }
+}
+</script>
---
+++ src/pages/Registrar.vue
@@ -0,0 +1,12 @@
+<template>
+    <CardForm heading="Registrar-se" isRegistration />
+ </template>
+ 
+ <script>
+     import CardForm from '@/components/CardForm.vue';
+ 
+     export default {
+     name: 'RegistroView',
+     components: { CardForm }
+ }
+ </script>
